9f06e6b7ea182a1d1426272cc020b8f2277d31ee
Daniel Lovera <daanilo2497@gmail.com>
2023-10-01 01:11:00 -0300

    Se elmina el codigo sobrante

--- app/index.ts
+++ app/index.ts
@@ -14,15 +14,10 @@ import { router } from './routers';
  *  - npm run dev "dev" (levanta el servidor en desarrollo para escuchar cambios)
  */
 
-// const spaceFlightRouter = require("./routes/spaceFlight");
 const PORT = 3000;
-// const TIMEOUT = 5*1000;
 
 const app = express();
 app.use(express.static("client")); // TODO: Eliminar, no creo que haga falta si no existe un directorio client con archivos que el servidor deba entregar.
 app.use(bodyParser.urlencoded({extended: false})); // TODO: Eliminar, no creo que haga falta, todos son verbos GET, no hay cuerpos en las peticiones.
-app.use('/', router);
-
-// app.use("/spaceflight_news", spaceFlightRouter);
-
+app.use('', router);
 app.listen(PORT, () => { console.log(`Server running at :${PORT}`)});
--- app/routers/index.ts
+++ app/routers/index.ts
@@ -1,23 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { pingRouter } from './ping_router';
 import { metarRouter } from './metars_router';
 import { articlesRouter } from './articles_router';
 import { quotesRouter } from './quotes_router';
-import { createErrorResponse } from '../utils/response_util';
-import httpStatus from 'http-status';
 
 const router = express.Router();
 router.use([pingRouter, metarRouter, articlesRouter, quotesRouter]);
-
-// Default controller
-router.get('*', (_request: Request, response: Response) => {
-    const error = { 
-        code: 'resource_not_found', 
-        error: 'Resource Not Found', 
-        message: 'The requested resource could not be found' 
-    };
-    const errorResponse = createErrorResponse(error);
-    response.status(httpStatus.NOT_FOUND).send(errorResponse);
-});
 
 export { router };
